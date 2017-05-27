b632b8f63a0ecd1eb5f9acb8f268021a2b888c06
erinl1 <erin.lee.ny@gmail.com>
2017-05-27 13:24:00 -0400

    S3 edit

--- s3-upload.rb
+++ s3-upload.rb
@@ -1,4 +1,20 @@
 require 'aws-sdk'
-s3 = Aws::S3::Resource.new(region:'us-east')
-obj = s3.bucket('eventrep-images').object(ENV['AWSSecretKey'])
-obj.upload_file('README.md')
+require 'dotenv'
+require 'aws/s3'
+Dotenv.load
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_KEY']
+)
+#s3 = Aws::S3::Client.new(region: 'us-east-2')
+#s3.create_bucket(bucket: 'images-raspberrypi')
+
+pictures = ['raspberry.jpg','pie.jpg']
+pictures.each do |x|
+  s3 = Aws::S3::Resource.new(region:'us-east-2')
+  obj = s3.bucket('images-raspberrypi').object(x)
+  obj.upload_file(x)
+end
+
+#s3 = Aws::S3::Client.new(region: 'us-east-1')
+#s3.create_bucket(bucket: 'eventrep')
