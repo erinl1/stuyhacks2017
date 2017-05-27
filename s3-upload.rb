a1501c56114b8f260543fae032032056719a24c3
erinl1 <erin.lee.ny@gmail.com>
2017-05-27 10:48:54 -0400

    Secret key

--- s3-upload.rb
+++ s3-upload.rb
@@ -1,4 +1,4 @@
 require 'aws-sdk'
 s3 = Aws::S3::Resource.new(region:'us-east')
-obj = s3.bucket('eventrep-images').object('DnTnG9vMj5J4vc+HhfxPAMO7oNZUDHreAVQxiehk')
+obj = s3.bucket('eventrep-images').object(ENV['AWSSecretKey'])
 obj.upload_file('README.md')
