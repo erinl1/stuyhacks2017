9822870d0052daff268065883dc42474f0506b80
erinl1 <erin.lee.ny@gmail.com>
2017-05-27 20:53:24 -0400

    Edit

--- s3-upload.rb
+++ s3-upload.rb
@@ -3,15 +3,13 @@ require 'dotenv'
 require 'aws/s3'
 require 's3'
 Dotenv.load
-service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                          :secret_access_key => ENV['AWS_SECRET_KEY'])
 AWS::S3::Base.establish_connection!(
   :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_KEY']
 )
 #USE FIRST TIME
-s3 = Aws::S3::Client.new(region: 'us-east-2')
-s3.create_bucket(bucket: 'images-raspberrypi')
+#s3 = Aws::S3::Client.new(region: 'us-east-2')
+#s3.create_bucket(bucket: 'images-raspberrypi')
 
 pictures = ['raspberry.jpg','mascot.png','pie.jpg']
 pictures.each do |x|
